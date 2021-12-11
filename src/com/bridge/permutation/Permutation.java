0eea4cb8bf0bc53b7ab9ac1d4f669d5a59fb79ee
Navneet <navn0306@gmail.com>
2021-12-11 18:00:17 +0530

    ADD: Feature to print all possible permutation for an entered string

--- src/com/bridge/permutation/Permutation.java
+++ src/com/bridge/permutation/Permutation.java
@@ -1,7 +1,38 @@
 package com.bridge.permutation;
 
+import java.util.Scanner;
+
 public class Permutation {
     public static void main(String[] args) {
-        System.out.println("\nWelcome, this program finds the all possible permutation for the program");
+
+        System.out.println("Enter a String :");
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = str.length();
+
+        Permutation permutation = new Permutation();
+        permutation.permute(str, 0, n - 1);
+    }
+
+    private void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.println(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    public String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 }
