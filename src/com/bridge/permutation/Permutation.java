052838b85e93ddb9f04d821f14fe3c478968418f
Navneet <navn0306@gmail.com>
2021-12-11 17:55:04 +0530

    ADD: Welcome message

---
+++ src/com/bridge/permutation/Permutation.java
@@ -0,0 +1,7 @@
+package com.bridge.permutation;
+
+public class Permutation {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, this program finds the all possible permutation for the program");
+    }
+}
